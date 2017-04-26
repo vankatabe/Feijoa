8baa02fb5bacd35c66ac575bb4af0075f73f5b0e
vankatabe <ivan@vankata.be>
2017-04-26 12:48:08 +0300

    CheckSiteLoad Test working

---
+++ Blog.UI.Tests/BrowserHost.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestStack.Seleno;
+using TestStack.Seleno.Configuration;
+
+namespace Blog.UI.Tests
+{
+    public static class BrowserHost
+    {
+        public static readonly SelenoHost Instance = new SelenoHost();
+        private static readonly string RootUrl = "http://localhost:60634/Article/List";
+
+        static BrowserHost()
+        {
+            Instance.Run("Blog", 60634);
+            // For Chrome: Instance.Run("Blog", 60634, w => w.WithRemoteWebDriver(BrowserFactory.Chrome));
+        }
+    }
+}

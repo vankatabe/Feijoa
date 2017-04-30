b7c33d2c8151ffc64b8909e53d212229c0f94fa1
vankatabe <ivan@vankata.be>
2017-04-30 13:28:17 +0300

    Changed port to 60639 in BrowserHost

--- Blog.UI.Tests/BrowserHost.cs
+++ Blog.UI.Tests/BrowserHost.cs
@@ -15,7 +15,7 @@ namespace Blog.UI.Tests
 
         static BrowserHost()
         {
-            Instance.Run("Blog", 60634);
+            Instance.Run("Blog", 60639);
             // For Chrome: Instance.Run("Blog", 60634, w => w.WithRemoteWebDriver(BrowserFactory.Chrome));
             // or Instance.Run("Blog", 60634, w => w.WithRemoteWebDriver(() => new ChromeDriver()));
 

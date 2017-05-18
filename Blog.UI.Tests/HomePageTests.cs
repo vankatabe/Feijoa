5e34512819c9115955a316e7c7e4accb16f6f2d2
vankatabe <ivan@vankata.be>
2017-05-18 11:34:28 +0300

    #18 Implemented: run all tests in a class in a single Blog browser

--- Blog.UI.Tests/HomePageTests.cs
+++ Blog.UI.Tests/HomePageTests.cs
@@ -14,43 +14,43 @@ namespace Blog.UI.Tests
     [TestFixture]
     public class HomePageTests
     {
-        private IWebDriver driver;
+        private IWebDriver driver = BrowserHost.Instance.Application.Browser;
 
         [SetUp]
         public void Init()
         {
-            this.driver = BrowserHost.Instance.Application.Browser;
+            //this.driver = BrowserHost.Instance.Application.Browser;
         }
 
         [TearDown]
         public void CleanUp()
         {
             // Add old-style logger for failed tests
-     //    if (TestContext.CurrentContext.Result.Outcome.Status == TestStatus.Failed)
-     //    {
-     //        string filenameTxt = AppDomain.CurrentDomain.BaseDirectory.Replace("bin\\Debug\\", string.Empty) + ConfigurationManager.AppSettings["Logs"] + TestContext.CurrentContext.Test.Name + ".txt";
-     //        if (File.Exists(filenameTxt))
-     //        {
-     //            File.Delete(filenameTxt);
-     //        }
-     //        File.WriteAllText(filenameTxt,
-     //            "Test full name:\t" + TestContext.CurrentContext.Test.FullName + "\r\n\r\n"
-     //            + "Work directory:\t" + TestContext.CurrentContext.WorkDirectory + "\r\n\r\n"
-     //            + "Pass count:\t" + TestContext.CurrentContext.Result.PassCount + "\r\n\r\n"
-     //            + "Result:\t" + TestContext.CurrentContext.Result.Outcome.ToString() + "\r\n\r\n"
-     //            + "Message:\t" + TestContext.CurrentContext.Result.Message);
-     //
-     //        var screenshot = ((ITakesScreenshot)this.driver).GetScreenshot();
-     //        var filenameJpg = AppDomain.CurrentDomain.BaseDirectory.Replace("bin\\Debug\\", string.Empty) + ConfigurationManager.AppSettings["Logs"] + TestContext.CurrentContext.Test.Name + ".jpg";
-     //        screenshot.SaveAsFile(filenameJpg, ScreenshotImageFormat.Jpeg);
-     //    }
-     //
-            driver.Quit(); // causes Firefox to crash
+            //    if (TestContext.CurrentContext.Result.Outcome.Status == TestStatus.Failed)
+            //    {
+            //        string filenameTxt = AppDomain.CurrentDomain.BaseDirectory.Replace("bin\\Debug\\", string.Empty) + ConfigurationManager.AppSettings["Logs"] + TestContext.CurrentContext.Test.Name + ".txt";
+            //        if (File.Exists(filenameTxt))
+            //        {
+            //            File.Delete(filenameTxt);
+            //        }
+            //        File.WriteAllText(filenameTxt,
+            //            "Test full name:\t" + TestContext.CurrentContext.Test.FullName + "\r\n\r\n"
+            //            + "Work directory:\t" + TestContext.CurrentContext.WorkDirectory + "\r\n\r\n"
+            //            + "Pass count:\t" + TestContext.CurrentContext.Result.PassCount + "\r\n\r\n"
+            //            + "Result:\t" + TestContext.CurrentContext.Result.Outcome.ToString() + "\r\n\r\n"
+            //            + "Message:\t" + TestContext.CurrentContext.Result.Message);
+            //
+            //        var screenshot = ((ITakesScreenshot)this.driver).GetScreenshot();
+            //        var filenameJpg = AppDomain.CurrentDomain.BaseDirectory.Replace("bin\\Debug\\", string.Empty) + ConfigurationManager.AppSettings["Logs"] + TestContext.CurrentContext.Test.Name + ".jpg";
+            //        screenshot.SaveAsFile(filenameJpg, ScreenshotImageFormat.Jpeg);
+            //    }
+            //
+            //driver.Quit(); // causes Firefox to crash
         }
 
         [Test]
-        [Property("Priority", 1), Property("Homepage test number:", 1)]
-        [Description("Navigate to Blog webb address, expected: open Blog homepage")]
+        [Property("Priority", 1), Property("Test scenario number:", 9), Property("Homepage test number:", 1)]
+        [Description("Navigate to Blog webb address, expected: open Blog homepage and Logo present")]
         [Author("vankatabe")]
         public void CheckWebSiteLoad_EnterBlogURL_OpenBlogHomePage()
         {
@@ -70,6 +70,23 @@ namespace Blog.UI.Tests
             // could be also like next row - Effect - from the Effect column in the Excel file. Need to add Effect property in RegistrationUser.cs
             asserter.Invoke(null, new object[] { regPage, user.Effect });
             */
+        }
+
+        [Test]
+        [Property("Priority", 1), Property("Test scenario number:", 9), Property("Homepage test number:", 1)]
+        [Description("Navigate to Blog webb address, expected: open Blog homepage and Logo present")]
+        [Author("vankatabe")]
+        public void CheckWebSiteLoad_EnterBlogURL_OpenBlogHomePageProba()
+        {
+            var homePage = new HomePage(this.driver);
+            //  IWebDriver driver = BrowserHost.Instance.Application.Browser;
+            // WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(60));
+
+            homePage.NavigateTo(homePage.URL);
+
+            // var logo = homePage.Wait.Until(w => w.FindElement(By.XPath("/html/body/div[1]/div/div[1]/a")));
+
+            homePage.AssertHomePageLogoDisplayed("SOFTUNI BLOG");
         }
     }
 }

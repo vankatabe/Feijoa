38d622146e7ea26c57706c5988691503801dda73
vankatabe <ivan@vankata.be>
2017-06-07 22:07:58 +0300

    Create Register negative tests using Gherkin and SpecFlow closes #31

---
+++ Blog.UI.Tests/SpecFlowTests/RegisterPageTests.feature
@@ -0,0 +1,35 @@
+﻿Feature: RegisterPageTests
+	As a Visitor
+	I want to be able to register an Account
+	So that I can Login to the System
+
+@Regression
+# This test does not pass because of the usage in *Steps.cs of the registerPage.FillRegistrationFormNegative method instead of registerPage.FillRegistrationForm method. But is added here for completion.
+Scenario: Register _ UniqueCredentials _ RegisterSuccessful
+	Given I am on the Registration page
+	When I fill-in the registration form is "Register_UniqueCredentials_RegisterSuccessful"
+	Then Message should be displayed is "Hello"
+
+@Negative
+Scenario: Register _ Without _ Email
+	Given I am on the Registration page
+	When I fill-in the registration form is "Register_Without_Email"
+	Then Message should be displayed is "The Email field is required."
+
+@Negative
+Scenario: Register _ Without _ FullName
+	Given I am on the Registration page
+	When I fill-in the registration form is "Register_Without_FullName"
+	Then Message should be displayed is "The Full Name field is required."
+
+@Negative
+Scenario: Register _ Without _ Password
+	Given I am on the Registration page
+	When I fill-in the registration form is "Register_Without_Password"
+	Then Message should be displayed is "The Password field is required."
+
+@Negative
+Scenario: Register _ Without _ ConfirmPassword
+	Given I am on the Registration page
+	When I fill-in the registration form is "Register_Without_ConfirmPassword"
+	Then Message should be displayed is "The password and confirmation password do not match."
---
+++ Blog.UI.Tests/SpecFlowTests/RegisterPageTestsSteps.cs
@@ -0,0 +1,45 @@
+﻿using Blog.UI.Tests.Attributes;
+using Blog.UI.Tests.Models;
+using Blog.UI.Tests.Pages.HomePage;
+using Blog.UI.Tests.Pages.RegisterPage;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+using System.Reflection;
+using System.Threading;
+using TechTalk.SpecFlow;
+
+namespace Blog.UI.Tests
+{
+    [Binding]
+    [LogResultToFileAttribute]
+    public class RegisterPageTestsSteps
+    {
+        private static IWebDriver driver = BrowserHost.Instance.Application.Browser;
+        RegisterPage registerPage = new RegisterPage(driver);
+        BlogPages page = AccessExcelData.GetTestData(TestContext.CurrentContext.Test.Name); // Get the current test method name (TestContext.CurrentContext.Test.Name = Register_UniqueCredentials_RegisterSuccessful) and use it as a Key in the xlsx file
+
+        [Given(@"I am on the Registration page")]
+        public void GivenIAmOnTheRegistrationPage()
+        {
+            registerPage.NavigateTo(registerPage.URL);
+        }
+
+        [When(@"I fill-in the registration form is ""(.*)""")]
+        public void WhenIFill_InTheRegistrationFormWithoutIs(string testName)
+        {
+            registerPage.FillRegistrationFormNegative(page);
+            Thread.Sleep(1000); // Necessary due to test execution speed
+        }
+
+        [Then(@"Message should be displayed is ""(.*)""")]
+        public void ThenErrorMessageForNamesShouldBeDisplayedIs(string errorMessage)
+        {
+            registerPage.AssertEmailErrorMessageExists(page.Effect);
+            // for the DataDriven Asserter:
+            MethodInfo asserter = typeof(RegisterPageAsserter).GetMethod(page.Asserter);
+            //could be also like next row -Effect - from the Effect column in the Excel file - what message or effect are we expecting
+            asserter.Invoke(null, new object[] { registerPage, page.Effect });
+        }
+    }
+}
